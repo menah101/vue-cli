a3129b508e0c2af7e08224e01ff3acc7c605572e
Le Van Quy <quy_le_van@bita.jp>
2022-06-23 22:44:00 +0700

    chore: create component and props

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ visible ? 'Hide' : 'Show' }} Details</button>
+
+    <ul v-if="visible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAddress'
+  ],
+  data() {
+    return {
+      visible: false,
+      friend: {
+        id: "halo",
+        name: "Halo",
+        phone: "123456789",
+        email: "halo-demo@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.visible = !this.visible; 
+    },
+  },
+};
+</script>
+
